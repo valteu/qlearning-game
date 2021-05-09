5457091a984b4ab8045b9d0160c0410e3373ad1e
valteu <62844981+valteu@users.noreply.github.com>
2021-05-09 23:46:30 +0200

    Update basic_program.py

--- basic_program.py
+++ basic_program.py
@@ -1,8 +1,8 @@
-import tensorflow as tf
 import pygame
 
 import time
 import sys
+import random
 
 pygame.init()
 pygame.font.init()
@@ -13,54 +13,100 @@ WIDTH = 800
 HEIGHT = 600
 
 class Player:
-	def __init__(self, pos):
-		self.pos = pos
-		self.raduis = 1
+    def __init__(self, pos):
+        self.pos = pos
+        self.radius = 1
+        self.distance = distance_to_goal(self.pos)[0]
+        self.new_distance = WIDTH
+        self.weightleft = 1
+        self.weightright = 1
 
-	def draw(self, screen):
-		x, y = self.pos
-		r = self.raduis * SCALE
-		pygame.draw.circle(screen, (255, 0, 0), (x, y - r), r)
+    def draw(self, screen):
+        x, y = self.pos
+        r = self.radius * SCALE
+        pygame.draw.circle(screen, (255, 0, 0), (x, y - r), r)
 
-	def update(self, duration, keys, objects, screen):
-		collisions = check_collision(objects, self, self.pos)
-		speed = 50
+    def update(self, objects, screen):
 
-		x, y = list(self.pos)
-		if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-		    x -= speed * duration
-		if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-		    x += speed * duration
-		# if keys[pygame.K_UP] or keys[pygame.K_w]:
-		#     y -= speed * duration
-		# if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-		#     y += speed * duration
-		self.pos[0] = x
-		# self.pos[1] = y
-		if check_collision(objects, self, [x, self.pos[1]]):
-		    print("goal achieved")
+        direction = "0"
+        speed = 5
+        objects = objects
+        x, y = list(self.pos)
+
+        action = random.uniform(0, 1)
+        if action <= ((1/2) * self.weightleft):
+            direction = "left"
+        elif action <= ((1) * self.weightright):
+            direction = "right"
+        # elif action <= ((1) * self.weightstand):
+        #     direction = "stand"
+
+        if direction == "left":
+            x -= speed
+        if direction == "right":
+            x += speed
+        if direction == "stand":
+            pass
+
+        self.new_distance = distance_to_goal(self.pos)
+        new_distance = int(self.new_distance[0])
+        if self.distance < new_distance:
+            print("wd")
+            if direction == "left":
+                self.weightleft = self.weightleft / 1.01
+
+            elif direction == "right":
+                self.weightright = self.weightright / 1.01
+
+        elif self.distance > new_distance:
+            print("bd")
+            if direction == "left":
+                self.weightleft = self.weightleft * 1.01
+
+            elif direction == "right":
+                self.weightright = self.weightright * 1.01
+
+        self.distance = new_distance
+        self.pos[0] = x
+            # self.pos[1] = y
+        print("l: ", self.weightleft)
+        print("r: ", self.weightright)
+        if check_collision(objects, self, [x, self.pos[1]]):
+            print("goal achieved")
 
 class Goal:
-	def __init__(self, pos):
-		self.pos = pos
-		self.width = 100
-		self.height = 600
+    def __init__(self, pos):
+        self.pos = pos
+        self.width = 100
+        self.height = 600
 
-	def draw(self, screen):
-		w = self.width
-		h = self.height
-		x = self.pos[0] - w
-		y = self.pos[1] - h
-		pygame.draw.rect(screen, (0, 255, 0), (x, y, w, h))
+    def draw(self, screen):
+        w = self.width
+        h = self.height
+        x = self.pos[0] - w
+        y = self.pos[1] - h
+        pygame.draw.rect(screen, (0, 255, 0), (x, y, w, h))
+    def update(self, objects, screen):
+        pass
 
-	def update(self, duration, keys, objects, screen):
-		pass
+def distance_to_goal(pos):
+    posx, posy = pos
+    goalx, goaly = goalpos
+
+    xdist = goalx - posx
+    ydist = goaly - posy
+    distance = [xdist, ydist]
+
+    return distance
+
 
 def load_level():
-	objects = []
-	objects.append(Player([WIDTH/2, HEIGHT]))
-	objects.append(Goal([WIDTH, HEIGHT]))
-	return objects
+    global goalpos
+    goalpos = [WIDTH, HEIGHT]
+    objects = []
+    objects.append(Player([WIDTH/2, HEIGHT]))
+    objects.append(Goal(goalpos))
+    return objects
 
 def check_collision(objects, mover, probe_pos):
     collisions = []
@@ -68,7 +114,7 @@ def check_collision(objects, mover, probe_pos):
         if obj is mover:
             continue
         dist_x = obj.pos[0] - probe_pos[0]
-        dist_y = obj.pos[1] - probe_pos[1]
+        # dist_y = obj.pos[1] - probe_pos[1]
         if probe_pos[0] < mover.pos[0] and dist_x > 0:  #left
             continue
         if probe_pos[0] > mover.pos[0] and dist_x < 0:  #right
@@ -77,16 +123,17 @@ def check_collision(objects, mover, probe_pos):
         #     continue
         # if probe_pos[1] > mover.pos[1] and dist_y < 0:  #down
         #     continue
-        if abs(dist_x) < 0.95 and abs(dist_y) < 0.95:
+        if abs(dist_x) < 0.95:
             collisions.append(obj)
-    return collisions	
-
+    return collisions   
 
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('ai controlled game')
     objects = load_level()
     # player = [obj for obj in objects if isinstance(obj, Player)][0]
+
+    goal = WIDTH
 
     last_time = time.perf_counter()
     run = True
@@ -101,15 +148,16 @@ def main():
         keys = pygame.key.get_pressed()
         if keys[pygame.K_ESCAPE]:
             run = False
-        if keys[pygame.K_r]:
-            replay()
+
         for obj in objects:
-            obj.update(1 / 60, keys, objects, screen)
+            obj.update(objects, screen)
+
         # Draw
 
         screen.fill((200, 200, 100))
         for obj in objects:
             obj.draw(screen)
+
 
         pygame.display.update()
 
