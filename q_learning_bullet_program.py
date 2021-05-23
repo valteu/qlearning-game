67792f733cca1dbfd6c0d8937b16d65daeb8c111
valteu <62844981+valteu@users.noreply.github.com>
2021-05-23 22:43:56 +0200

    randomized shoot system

--- q_learning_bullet_program.py
+++ q_learning_bullet_program.py
@@ -15,7 +15,7 @@ WIDTH = 800
 HEIGHT = 800
 
 BULLET_SHOT = False
-AGENT_POS = [20, 400]
+AGENT_POS = [20, int(HEIGHT / 2 - HEIGHT / 20)]
 
 HM_EPISODES = 50000
 MOVE_PENALTY = 1
@@ -35,7 +35,7 @@ class Enemy:
 		self.pos = pos
 		self.width = int(WIDTH / 10)
 		self.height = int(HEIGHT / 10)
-		self.speed = int(HEIGHT / 50)
+		self.speed = int(HEIGHT / 10)
 
 	def draw(self, screen):
 		x, y = self.pos
@@ -43,13 +43,13 @@ class Enemy:
 		h = self.height
 		pygame.draw.rect(screen, (255, 0, 0), (x, y, w, h))
 
-	def update(self, duration):
+	def update(self, duration, objects):
 		direction = np.random.randint(-1, 2)
-		if direction == -1:
+		if direction == -1 and int(self.pos[1] - self.speed) >= 0:
 			self.pos[1] -= self.speed
 		elif direction == 0:
 			pass
-		elif direction == 1:
+		elif direction == 1 and int(self.pos[1] + self.speed) <= WIDTH:
 			self.pos[1] += self.speed
 
 class Friend:
@@ -58,7 +58,7 @@ class Friend:
 		self.pos = pos
 		self.width = int(WIDTH / 10)
 		self.height = int(HEIGHT / 10)
-		self.speed = int(HEIGHT / 50)
+		self.speed = int(HEIGHT / 10)
 
 	def draw(self, screen):
 		x, y = self.pos
@@ -66,13 +66,13 @@ class Friend:
 		h = self.height
 		pygame.draw.rect(screen, (0, 255, 0), (x, y, w, h))
 
-	def update(self, duration):
+	def update(self, duration, objects):
 		direction = np.random.randint(-1, 2)
-		if direction == -1:
+		if direction == -1 and int(self.pos[1] - self.speed) >= 0:
 			self.pos[1] -= self.speed
 		elif direction == 0:
 			pass
-		elif direction == 1:
+		elif direction == 1 and int(self.pos[1] + self.speed) <= WIDTH:
 			self.pos[1] += self.speed
 
 class Agent:
@@ -89,13 +89,38 @@ class Agent:
 		h = self.height
 		pygame.draw.rect(screen, (0, 0, 255), (x, y, w, h))
 
-	def update(self, duration):
+	def update(self, duration, objects):
 		pass
 
 	def shoot(self, aim, objects):
-		bullet = Bullet(self.pos, aim)
+		action = np.random.randint(0, 10) #10 actions
+		if action == 0:
+			target = [600, HEIGHT / 10]
+		elif action == 1:
+			target = [600, 2 * HEIGHT / 10]
+		elif action == 2:
+			target = [600, 3 * HEIGHT / 10]
+		elif action == 3:
+			target = [600, 4 * HEIGHT / 10]
+		elif action == 4:
+			target = [600, 5 * HEIGHT / 10]
+		elif action == 5:
+			target = [600, 6 * HEIGHT / 10]
+		elif action == 6:
+			target = [600, 7 * HEIGHT / 10]
+		elif action == 7:
+			target = [600, 8 * HEIGHT / 10]
+		elif action == 8:
+			target = [600, 9 * HEIGHT / 10]
+		elif action == 9:
+			target = [600, 10 * HEIGHT / 10]
+
+		global bullet
+		startpos = [int(self.pos[0] + self.width / 2), int(HEIGHT / 2)]
+		print(startpos)
+
+		bullet = Bullet(startpos, target)
 		objects.append(bullet)
-		BULLET_SHOT = True
 
 class Bullet:
 
@@ -103,30 +128,38 @@ class Bullet:
 		self.x, self.y = pos
 		self.radius = 20
 		self.aim = aim
-		self.speed = 15
+		self.speed = 50
 		self.angle = math.atan2(aim[1] - pos[1], aim[0] - pos[0]) #angle to target in radians
 		self.dx = math.cos(self.angle) * self.speed
 		self.dy = math.sin(self.angle) * self.speed
 		self.cx, self.cy = pos
 
 	def draw(self, screen):
-		# print(x, y)
 		r = self.radius
-		pygame.draw.circle(screen, (0, 0, 255), (self.x, self.y), r)
+		pygame.draw.circle(screen, (0, 0, 0), (self.x, self.y), r)
 
-	def update(self, duration):
+	def update(self, duration, objects):
 		self.cx += self.dx
 		self.cy += self.dy
 		self.x = int(self.cx)
 		self.y = int(self.cy)
-
+		if 0 <= self.x <= WIDTH: 
+			pass
+		else:
+			# objects.remove(bullet)
+			BULLET_SHOT = False
+		if 0 <= self.y <= HEIGHT:
+			pass
+		else:
+			# objects.remove(bullet)
+			BULLET_SHOT = False
 	
-def main(BULLET_SHOT):
+def main():
 	screen = pygame.display.set_mode((WIDTH, HEIGHT))
 	pygame.display.set_caption('ai aim-predicting game')
-	enemy = Enemy([100, 400])
+	enemy = Enemy([600, 240])
 	agent = Agent(AGENT_POS)
-	friend = Friend([100, 150])
+	friend = Friend([600, 480])
 	objects = [friend, enemy, agent]
 	is_clicked = False
 
@@ -137,6 +170,7 @@ def main(BULLET_SHOT):
 	#                 for iiii in range(-SIZE+1, SIZE):
 	#                     q_table[((i, ii), (iii, iiii))] = [np.random.uniform(-5, 0) for i in range(4)]
 	show = True
+	BULLET_SHOT = False
 	while show:
 		last_time = time.perf_counter()
 		duration = time.perf_counter() - last_time
@@ -156,12 +190,13 @@ def main(BULLET_SHOT):
 			tupleaim = pygame.mouse.get_pos()
 			aim = list(tupleaim)
 			agent.shoot(aim, objects)
+			BULLET_SHOT = True
 			
 		if click[0] == 0 and is_clicked == True:
 			is_clicked = False
 
 		for obj in objects:
-			obj.update(duration)
+			obj.update(duration, objects)
 
 		screen.fill((200, 200, 200))
 		for obj in objects:
@@ -173,4 +208,4 @@ def main(BULLET_SHOT):
 		sys.stdout.flush()
 
 
-main(BULLET_SHOT)
+main()
