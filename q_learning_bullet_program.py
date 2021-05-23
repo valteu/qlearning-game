4cc68b896eaceff600604fe570853122c9fcb053
valteu <62844981+valteu@users.noreply.github.com>
2021-05-23 17:07:09 +0200

    fixed bullet shooting, friend class added

--- q_learning_bullet_program.py
+++ q_learning_bullet_program.py
@@ -52,6 +52,29 @@ class Enemy:
 		elif direction == 1:
 			self.pos[1] += self.speed
 
+class Friend:
+
+	def __init__(self, pos):
+		self.pos = pos
+		self.width = int(WIDTH / 10)
+		self.height = int(HEIGHT / 10)
+		self.speed = int(HEIGHT / 50)
+
+	def draw(self, screen):
+		x, y = self.pos
+		w = self.width
+		h = self.height
+		pygame.draw.rect(screen, (0, 255, 0), (x, y, w, h))
+
+	def update(self, duration):
+		direction = np.random.randint(-1, 2)
+		if direction == -1:
+			self.pos[1] -= self.speed
+		elif direction == 0:
+			pass
+		elif direction == 1:
+			self.pos[1] += self.speed
+
 class Agent:
 
 	def __init__(self, pos):
@@ -64,52 +87,38 @@ class Agent:
 		x, y = self.pos
 		w = self.width
 		h = self.height
-		pygame.draw.rect(screen, (0, 255, 0), (x, y, w, h))
+		pygame.draw.rect(screen, (0, 0, 255), (x, y, w, h))
 
 	def update(self, duration):
 		pass
 
+	def shoot(self, aim, objects):
+		bullet = Bullet(self.pos, aim)
+		objects.append(bullet)
+		BULLET_SHOT = True
+
 class Bullet:
 
-	def __init__(self, startpos ,aim):
-		self.startpos = startpos
+	def __init__(self, pos ,aim):
+		self.x, self.y = pos
 		self.radius = 20
 		self.aim = aim
-		self.path = 0
-		self.step = 0
 		self.speed = 15
-		self.pos = startpos
-		self.path_len = 0
-		self.x_step = 0
+		self.angle = math.atan2(aim[1] - pos[1], aim[0] - pos[0]) #angle to target in radians
+		self.dx = math.cos(self.angle) * self.speed
+		self.dy = math.sin(self.angle) * self.speed
+		self.cx, self.cy = pos
 
 	def draw(self, screen):
-		x, y = self.pos
 		# print(x, y)
 		r = self.radius
-		pygame.draw.circle(screen, (0, 0, 255), (x, y), r)
-
-	def built_path(self):
-		x2 = self.aim[0] 
-		y2 = self.aim[1]
-		x1 = self.pos[0] 
-		y1 = self.pos[1]
-
-		self.deltax = x2 - x1
-		self.deltay = abs(-y2 - -y1)
-
-		self.path_len = math.sqrt(self.deltax * self.deltax + self.deltay * self.deltay)
-
-		self.steps = int(self.path_len / self.speed)
-		self.path = self.deltay / self.deltax
-		# print(self.path)
-		self.x_step = self.deltax / self.steps
+		pygame.draw.circle(screen, (0, 0, 255), (self.x, self.y), r)
 
 	def update(self, duration):
-		self.pos[0] = (self.x_step * self.step) + self.startpos[0]
-		self.pos[1] = (self.path * (self.x_step * self.step)) * -1 - self.startpos[1]
-		print(self.step)
-		self.step += 1
-
+		self.cx += self.dx
+		self.cy += self.dy
+		self.x = int(self.cx)
+		self.y = int(self.cy)
 
 	
 def main(BULLET_SHOT):
@@ -117,7 +126,8 @@ def main(BULLET_SHOT):
 	pygame.display.set_caption('ai aim-predicting game')
 	enemy = Enemy([100, 400])
 	agent = Agent(AGENT_POS)
-	objects = [enemy, agent]
+	friend = Friend([100, 150])
+	objects = [friend, enemy, agent]
 	is_clicked = False
 
 	# q_table = {}
@@ -145,16 +155,13 @@ def main(BULLET_SHOT):
 			is_clicked = True
 			tupleaim = pygame.mouse.get_pos()
 			aim = list(tupleaim)
-			bullet = Bullet([20, 400], aim)
-			objects.append(bullet)
-			bullet.built_path()
-			BULLET_SHOT = True
+			agent.shoot(aim, objects)
+			
 		if click[0] == 0 and is_clicked == True:
 			is_clicked = False
 
-		enemy.update(duration)
-		if len(objects) > 2:
-			bullet.update(duration)
+		for obj in objects:
+			obj.update(duration)
 
 		screen.fill((200, 200, 200))
 		for obj in objects:
