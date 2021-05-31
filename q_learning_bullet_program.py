fdaac7de2cae14b305f6ab7fa56d0f168fe5d1b7
valteu <62844981+valteu@users.noreply.github.com>
2021-05-31 17:05:53 +0200

    added q_table, fixes necessary
    fixes necessary for running the program

--- q_learning_bullet_program.py
+++ q_learning_bullet_program.py
@@ -17,15 +17,18 @@ HEIGHT = 800
 BULLET_SHOT = False
 AGENT_POS = [20, int(HEIGHT / 2 - HEIGHT / 20)]
 
-HM_EPISODES = 50000
-MOVE_PENALTY = 1
-ENEMY_PENALTY = 300
-FOOD_REWARD = 25
+DISCRETE_OS_SIZE = [10]
+
+EPISODES = 25000
+SHOOT_PENALTY = 1
+ENEMY_HIT = 25
+FRIEND_HIT = 300
 epsilon = 0.9
 EPS_DECAY = 0.9998 
-SHOW_EVERY = 10000
-MAX_STEPS = 200
-
+SHOW_EVERY = 5000
+MAX_STEPS = 20
+posstate = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+actions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 LEARNING_RATE = 0.1
 DISCOUNT = 0.95
 
@@ -36,6 +39,7 @@ class Enemy:
 		self.width = int(WIDTH / 10)
 		self.height = int(HEIGHT / 10)
 		self.speed = int(HEIGHT / 10)
+		self.posstate = pos[1] / 80
 
 	def draw(self, screen):
 		x, y = self.pos
@@ -51,6 +55,7 @@ class Enemy:
 			pass
 		elif direction == 1 and int(self.pos[1] + self.speed) <= WIDTH:
 			self.pos[1] += self.speed
+		self.posstate = self.pos[1] / 80
 
 class Friend:
 
@@ -59,6 +64,7 @@ class Friend:
 		self.width = int(WIDTH / 10)
 		self.height = int(HEIGHT / 10)
 		self.speed = int(HEIGHT / 10)
+		self.posstate = pos[1] / 80
 
 	def draw(self, screen):
 		x, y = self.pos
@@ -74,6 +80,7 @@ class Friend:
 			pass
 		elif direction == 1 and int(self.pos[1] + self.speed) <= WIDTH:
 			self.pos[1] += self.speed
+		self.posstate = self.pos[1] / 80
 
 class Agent:
 
@@ -81,6 +88,7 @@ class Agent:
 		self.pos = pos
 		self.width = int(WIDTH / 10)
 		self.height = int(HEIGHT / 10)
+		self.posstate = np.random.randint(0, 10)
 
 	def draw(self, screen):
 		self.pos
@@ -92,35 +100,50 @@ class Agent:
 	def update(self, duration, objects):
 		pass
 
-	def shoot(self, aim, objects):
-		action = np.random.randint(0, 10) #10 actions
+	def shoot(self, objects, actioninput=False):
+		if actioninput == False:
+			action = np.random.randint(0, 10) #10 actions
+		else:
+			action = actioninput
 		if action == 0:
-			target = [600, HEIGHT / 10]
+			target = [600, 0 * HEIGHT / 10]
+			self.posstate = posstate[0]
 		elif action == 1:
-			target = [600, 2 * HEIGHT / 10]
+			target = [600, 1 * HEIGHT / 10]
+			self.posstate = posstate[1]
 		elif action == 2:
-			target = [600, 3 * HEIGHT / 10]
+			target = [600, 2 * HEIGHT / 10]
+			self.posstate = posstate[2]
 		elif action == 3:
-			target = [600, 4 * HEIGHT / 10]
+			target = [600, 3 * HEIGHT / 10]
+			self.posstate = posstate[3]
 		elif action == 4:
-			target = [600, 5 * HEIGHT / 10]
+			target = [600, 4 * HEIGHT / 10]
+			self.posstate = posstate[4]
 		elif action == 5:
-			target = [600, 6 * HEIGHT / 10]
+			target = [600, 5 * HEIGHT / 10]
+			self.posstate = posstate[5]
 		elif action == 6:
-			target = [600, 7 * HEIGHT / 10]
+			target = [600, 6 * HEIGHT / 10]
+			self.posstate = posstate[6]
 		elif action == 7:
-			target = [600, 8 * HEIGHT / 10]
+			target = [600, 7 * HEIGHT / 10]
+			self.posstate = posstate[7]
 		elif action == 8:
-			target = [600, 9 * HEIGHT / 10]
+			target = [600, 8 * HEIGHT / 10]
+			self.posstate = posstate[8]
 		elif action == 9:
-			target = [600, 10 * HEIGHT / 10]
+			target = [600, 9 * HEIGHT / 10]
+			self.posstate = posstate[9]
 
 		global bullet
 		startpos = [int(self.pos[0] + self.width / 2), int(HEIGHT / 2)]
-		print(startpos)
 
 		bullet = Bullet(startpos, target)
 		objects.append(bullet)
+
+	def __sub__(self, other):
+		return (self.posstate-other.posstate)
 
 class Bullet:
 
@@ -154,58 +177,98 @@ class Bullet:
 			# objects.remove(bullet)
 			BULLET_SHOT = False
 	
-def main():
+def main(epsilon):
 	screen = pygame.display.set_mode((WIDTH, HEIGHT))
-	pygame.display.set_caption('ai aim-predicting game')
-	enemy = Enemy([600, 240])
-	agent = Agent(AGENT_POS)
-	friend = Friend([600, 480])
-	objects = [friend, enemy, agent]
-	is_clicked = False
+	pygame.display.set_caption('ai aim game')
 
-	# q_table = {}
-	# for i in range(-SIZE+1, SIZE):
-	#     for ii in range(-SIZE+1, SIZE):
-	#         for iii in range(-SIZE+1, SIZE):
-	#                 for iiii in range(-SIZE+1, SIZE):
-	#                     q_table[((i, ii), (iii, iiii))] = [np.random.uniform(-5, 0) for i in range(4)]
-	show = True
+	q_table = {}
+	for i in range(-len(posstate)+1, len(posstate)):
+		for ii in range(-len(posstate)+1, len(posstate)):
+			q_table[(i, ii)] = [np.random.uniform(-5, 0) for i in range(len(actions))]
+
+	print(q_table)
+	show = False
 	BULLET_SHOT = False
-	while show:
-		last_time = time.perf_counter()
-		duration = time.perf_counter() - last_time
-		last_time += duration
+	episode_rewards = []
+	for episode in range(EPISODES):
+		enemy = Enemy([600, 240])
+		agent = Agent(AGENT_POS)
+		friend = Friend([600, 480])
+		objects = [friend, enemy, agent]
 
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				show = False
-				pygame.quit()
-		keys = pygame.key.get_pressed()
-		# if keys[pygame.K_SPACE] and BULLET_SHOT == False:
-		# 	objects.append(Bullet([600, 300]))
-		# 	BULLET_SHOT = True
-		click = pygame.mouse.get_pressed()
-		if click[0] == 1 and is_clicked == False:
-			is_clicked = True
-			tupleaim = pygame.mouse.get_pos()
-			aim = list(tupleaim)
-			agent.shoot(aim, objects)
-			BULLET_SHOT = True
-			
-		if click[0] == 0 and is_clicked == True:
-			is_clicked = False
+		if episode % SHOW_EVERY == 0:
+			show = True
+			print("#########################")
+		else:
+			show = False
 
-		for obj in objects:
-			obj.update(duration, objects)
+		episode_reward = 0
+		for i in range(MAX_STEPS):
+			obs = (int(agent-friend), int(agent-enemy))
+			if np.random.uniform() > epsilon:
+				action = np.argmax(q_table[obs])
+			else:
+				action = np.random.randint(0, 10)
+			print(np.argmax(q_table[obs]))
 
-		screen.fill((200, 200, 200))
-		for obj in objects:
-			obj.draw(screen)
+			agent.shoot(objects, action)
 
-		pygame.display.update()
-		pygame.time.delay(50)
-		time.sleep(max(0, 1 / 60 - (time.perf_counter() - last_time)))
-		sys.stdout.flush()
+			if agent.posstate == enemy.posstate:
+				reward = ENEMY_HIT
+			elif agent.posstate == friend.posstate:
+				reward = -FRIEND_HIT
+			else:
+				reward = -SHOOT_PENALTY
 
+			new_obs = (agent-friend, agent-enemy)
+			max_future_q = np.max(q_table[new_obs])
+			current_q = q_table[obs][action]
 
-main()
+			if reward == ENEMY_HIT:
+				new_q = ENEMY_HIT
+			elif reward == -FRIEND_HIT:
+				new_q = -FRIEND_HIT
+			else:
+				new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+			q_table[obs][action] = new_q
+
+			if show:
+				print("=================")
+				print("=================")
+				print("=================")
+				print("=================")
+				print("=================")
+				print("=================")
+				print("=================")
+				print("=================")
+				last_time = time.perf_counter()
+				duration = time.perf_counter() - last_time
+				last_time += duration
+
+				for event in pygame.event.get():
+					if event.type == pygame.QUIT:
+						show = False
+						pygame.quit()
+					
+
+				for obj in objects:
+					obj.update(duration, objects)
+
+				screen.fill((200, 200, 200))
+				for obj in objects:
+					obj.draw(screen)
+
+				pygame.display.update()
+				pygame.time.delay(50)
+				time.sleep(max(0, 1 / 60 - (time.perf_counter() - last_time)))
+				sys.stdout.flush()
+			episode_reward += reward
+			if reward == ENEMY_HIT or reward == -FRIEND_HIT:
+				break
+		episode_rewards.append(episode_reward)
+		epsilon *= EPS_DECAY
+
+	oving_avg = np.convolve(episode_rewards, np.ones((SHOW_EVERY,))/SHOW_EVERY, mode='valid')
+	print(oving_avg)
+
+main(epsilon)
